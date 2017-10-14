365fe5780d1a64b1b38a6309875f288fc0dc047a
Jorge <pichardojorge243@gmail.com>
2017-10-13 20:30:18 -0500

    Initial commit

---
+++ src/main/java/org/jorge/primerabase/PrimerabaseApplication.java
@@ -0,0 +1,12 @@
+package org.jorge.primerabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerabaseApplication.class, args);
+	}
+}
